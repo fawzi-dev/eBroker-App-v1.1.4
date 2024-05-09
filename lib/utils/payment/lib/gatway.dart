87ceb5674de39b30c673f8acf6b6de44afa50e0d
Fawzi Gharib <fawz11gharib@gmail.com>
2024-05-09 13:34:06 +0300

    initial commit

---
+++ lib/utils/payment/lib/gatway.dart
@@ -0,0 +1,11 @@
+import 'package:empirekurd/utils/payment/lib/payment.dart';
+
+///This is Modal to store Payment gatway with its key
+class Gatway {
+  final String key;
+  final Payment instance;
+  Gatway({
+    required this.key,
+    required this.instance,
+  });
+}
